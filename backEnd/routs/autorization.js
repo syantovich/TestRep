c0658da6eab27d8b70f384a30bff3f7c11f045a1
syantovich <72738946+syantovich@users.noreply.github.com>
2022-02-02 23:11:55 +0300

    first

--- backEnd/routs/autorization.js
+++ backEnd/routs/autorization.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const {Router}=require("express")
 
 const router=Router();
@@ -9,16 +8,4 @@ router.post("/",(req,res)=>{
         isHome:true,
     })
 })
-=======
-const {Router}=require("express")
-
-const router=Router();
-
-router.post("/",(req,res)=>{
-    res.render("index",{
-        title:"Log in",
-        isHome:true
-    })
-})
->>>>>>> 8fbba4c2641bba6af4bf4bde0d98efd2972fbf93
 module.exports = router;
