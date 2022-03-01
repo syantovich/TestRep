605ef579017550abe56533243173592a1c201511
syantovich <72738946+syantovich@users.noreply.github.com>
2022-03-01 23:04:21 +0300

    made mane page, navbar

--- frontEnd/tickets-app/src/components/Film/Film.js
+++ frontEnd/tickets-app/src/components/Film/Film.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import "./Film.scss";
 
-const Film = (props) => {
-  const start = useState(new Date(props.dateStart));
-  const end = useState(new Date(props.dateEnd));
+const Film = ({ img, dateEnd, dateStart, name }) => {
+  const start = useState(new Date(dateStart));
+  const end = useState(new Date(dateEnd));
   console.log(typeof start[0]);
   return (
     <div className="wraper">
-      <img src={props.img} />
-      <span>{props.name}</span>
+      <img src={img} />
+      <span>{name}</span>
       <span>{`C ${start[0].getDay()}.${start[0].getMonth()}.${start[0].getFullYear()} по ${end[0].getDay()}.${end[0].getMonth()}.${start[0].getFullYear()}`}</span>
     </div>
   );
